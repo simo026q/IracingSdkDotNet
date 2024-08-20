7026cbb82714279966d063bf434992c1c18f1119
Simon Christensen <sbc@scantalk.com>
2024-08-20 12:41:58 +0200

    Add serialization benchmarks

---
+++ tests/IracingSdkDotNet.Serialization.Benchmarks/Program.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using IracingSdkDotNet.Serialization.Internal.Yaml;
+using IracingSdkDotNet.Serialization.Models.Session;
+using irsdkSharp.Serialization.Models.Session;
+using YamlDotNet.Core;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    public string Yaml { get; } = GetYaml();
+
+    [Benchmark]
+    public IracingSessionModel? Deserialize()
+    {
+        return (IracingSessionModel?)YamlSerializer.Deserialize(new Parser(new StringReader(Yaml)), typeof(IracingSessionModel), YamlSerializerOptions.Default);
+    }
+
+    [Benchmark]
+    public IRacingSessionModel? DeserializeIrsdk()
+    {
+        return IRacingSessionModel.Serialize(Yaml);
+    }
+
+    private static string? GetSolutionDirectory()
+    {
+        var directory = new DirectoryInfo(AppContext.BaseDirectory);
+
+        while (directory != null && !directory.GetFiles("*.sln").Any())
+        {
+            directory = directory.Parent;
+        }
+
+        return directory?.FullName;
+    }
+
+    private static string GetYaml()
+    {
+        var solutionDirectory = GetSolutionDirectory();
+        if (solutionDirectory == null)
+        {
+            throw new InvalidOperationException("Solution directory not found.");
+        }
+
+        var path = Path.Combine(solutionDirectory, "data", "le-mans porsche-963-gtp", "session-info.yaml");
+        return File.ReadAllText(path);
+    }
+}
